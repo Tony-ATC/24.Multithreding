0135bc106bdc870f41f0b2029430de0f4332b345
Tony-ATC <anthony.calascibetta@student.intecbrussel.be>
2021-01-21 23:23:15 +0100

    Chapter 24 - MultiThreading Exercises 1 - 4
    Initial commit

---
+++ src/CharacterPrinter.java
@@ -0,0 +1,16 @@
+public class CharacterPrinter implements Runnable {
+    private char c;
+    private int count;
+
+    public CharacterPrinter(char c, int count){
+        this.c = c;
+        this.count = count;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < count; i++) {
+            System.out.println(c);
+        }
+    }
+}
---
+++ src/PrintAppLambadadadada.java
@@ -0,0 +1,22 @@
+public class PrintAppLambadadadada {
+    public static void main(String[] args) {
+        Thread thread1 = new Thread(() -> print('#', 100));
+        Thread thread2 = new Thread(() -> print('*', 100));
+        System.out.println(thread1.getState());
+        System.out.println(thread2.getState());
+        thread1.setDaemon(true);
+        thread2.setDaemon(true);
+        thread1.start();
+        System.out.println(thread1.getState());
+        thread2.start();
+        System.out.println(thread2.getState());
+        print('@', 100);
+    }
+
+    public static void print(char c, int count){
+        for (int i = 0; i < count; i++) {
+            System.out.print(c);
+            Thread.yield();
+        }
+    }
+}
